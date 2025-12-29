84e7ed9141b0fe3acce10ed79994e0a32c54a566
Avinash Prasad <avionmission@gmail.com>
2025-12-29 21:35:18 +0530

    feat: Add getTaskList and updateTaskList endpoints

--- src/main/java/com/avionmission/tasks/controllers/TaskListController.java
+++ src/main/java/com/avionmission/tasks/controllers/TaskListController.java
@@ -7,6 +7,8 @@ import com.avionmission.tasks.services.TaskListService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(path = "/task-lists")
@@ -28,16 +30,29 @@ public class TaskListController {
                 .toList();
     }
 
-    @GetMapping("/test")
-    public String test() {
-        return "ALL OK!!";
-    }
-
     @PostMapping
     public TaskListDto createTaskList(@RequestBody TaskListDto taskListDto) {
         TaskList createdTaskList = taskListService.createTaskList(
                 taskListMapper.fromDto(taskListDto)
         );
         return  taskListMapper.toDto(createdTaskList);
+    }
+
+    @GetMapping(path = "/{task_list_id}")
+    public Optional<TaskListDto> getTaskList(@PathVariable("task_list_id") UUID taskListId) {
+        return taskListService.getTaskList(taskListId).map(taskListMapper::toDto);
+    }
+
+    @PutMapping(path = "/{task_list_id}")
+    public TaskListDto updateTaskList(
+            @PathVariable("task_list_id") UUID taskListId,
+            @RequestBody TaskListDto taskListDto
+    ) {
+        TaskList updatedTaskList = taskListService.updateTaskList(
+                taskListId,
+                taskListMapper.fromDto(taskListDto)
+        );
+
+        return taskListMapper.toDto(updatedTaskList);
     }
 }
--- src/main/java/com/avionmission/tasks/services/TaskListService.java
+++ src/main/java/com/avionmission/tasks/services/TaskListService.java
@@ -3,9 +3,13 @@ package com.avionmission.tasks.services;
 import com.avionmission.tasks.domain.entities.TaskList;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 
 public interface TaskListService {
     List<TaskList> listTaskLists();
     TaskList createTaskList(TaskList taskList);
+    Optional<TaskList> getTaskList(UUID id);
+    TaskList updateTaskList(UUID taskListId, TaskList taskList);
 }

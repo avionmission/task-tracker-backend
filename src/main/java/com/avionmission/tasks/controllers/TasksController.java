472cab761b48f8ade24c84b05e29976c24c77fca
Avinash Prasad <avionmission@gmail.com>
2025-12-31 21:16:52 +0530

    feat: implement updateTask endpoint

--- src/main/java/com/avionmission/tasks/controllers/TasksController.java
+++ src/main/java/com/avionmission/tasks/controllers/TasksController.java
@@ -38,6 +38,22 @@ public class TasksController {
         return taskMapper.toDto(createdTask);
     }
 
+    @GetMapping(path = "/{task_id}")
+    public Optional<TaskDto> getTask (
+            @PathVariable("task_list_id") UUID taskListId,
+            @PathVariable("task_id") UUID taskId
+    ){
+        return taskService.getTask(taskListId, taskId).map(taskMapper::toDto);
+    }
 
+    @PutMapping(path = "/{task_id}")
+    public TaskDto updateTask(
+            @PathVariable("task_list_id") UUID taskListId,
+            @PathVariable("task_id") UUID taskId,
+            @RequestBody TaskDto taskDto
+    ) {
+        Task updatedTask = taskService.updateTask(taskListId, taskId, taskMapper.fromDto(taskDto));
+        return taskMapper.toDto(updatedTask);
+    }
 
 }
